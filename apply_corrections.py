858a9465741f66b8ac61105854c04d70f1bc6d15
Chris Johnson <raugturi@gmail.com>
2012-09-05 12:37:49 -0400

    copy info from READMEN into script and shorten the variable descriptions

--- apply_corrections.py
+++ apply_corrections.py
@@ -16,8 +16,51 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+# apply_corrections
+#
+# A weechat plugin to re-print a user's messages with corrections applied when
+# they send a correction string (ex: s/typo/replacement).
+#
+# The following options are availalble:
+#
+# check_every: Interval, in seconds, between each check for expired messages.
+#              If set to 0 no check will be performed and all messages will be
+#              saved indefinitely.
+#
+# data_timeout: Time, in seconds, before a message is expired. If set to 0
+#               messages will never expire.
+#
+# message_limit: Number of messages to store per nick. If set to 0 all messages
+#                will be saved until they expire.
+#
+# print_format: Format string for the printed corrections.
+#               Default: "[nick]: [corrected]"
+#
+#               Variables allowed:
+#
+#               [nick]:        The nick of the person who sent the messages.
+#               [corrected]:   The corrected text of the previous message(s).
+#               [correction]:  The correction (format: s/typo/replacement).
+#               [original]:    The original message before correction.
+#               [pattern]:     The "typo" portion of the correction.
+#               [replacement]: The "replacement" portion of the correction.
+#               [timestamp]:   The timestamp of the original message.
+#
+# print_limit: Maximum number of lines to correct and print to the buffer. If
+#              set to 0 all lines that match the pattern will be printed.
+#
+# Note: Setting check_every, data_timeout, message_limit, or some combination
+# thereof to 0's will eat a lot of memory very quickly as it will drastically
+# increase the size of the dictionary holding previous messages.  Likewise,
+# setting print_limit to 0 with a large number of saved messages will quickly
+# fill your screen with a bunch of stuff should someone submit a generic
+# enough correction string.
+
 # History:
 #
+# 2012-09-05, Chris Johnson <raugturi@gmail.com>:
+#     version 0.6: copy info from README into script and shorten the variable
+#                  descriptions.
 # 2012-09-01, Chris Johnson <raugturi@gmail.com>:
 #     version 0.5: don't log the reprinted messages
 # 2012-08-31, Chris Johnson <raugturi@gmail.com>:
@@ -262,38 +305,19 @@ def desc_options():
     """
 
     weechat.config_set_desc_plugin(
-            'check_every',
-            'Interval, in seconds, between each check for expired messages.  '\
-            'If set to 0 no check will be performed and all messages will be '\
-            'saved indefinitely.  This will most likely use a lot of memory.')
+            'check_every', 'Interval between each check for expired messages.')
 
     weechat.config_set_desc_plugin(
-            'data_timeout',
-            'Time, in seconds, before a message is expired.  '\
-            'If set to 0 messages will never expire.  This will most likely '\
-            'use a lot of memory.')
+            'data_timeout', 'Time before a message is expired.')
 
     weechat.config_set_desc_plugin(
-            'message_limit',
-            'Number of messages to store per nick.  '\
-            'If set to 0 all messages will be saved until they expire.')
+            'message_limit', 'Number of messages to store per nick.')
 
     weechat.config_set_desc_plugin(
-            'print_format',
-            'Format string for the printed corrections (Default: "[nick]: '\
-            '[corrected]"). Variables allowed:\n'\
-            '       [nick]: The nick of the person who sent the messages.\n'\
-            '  [corrected]: The corrected text of the previous message(s).\n'
-            ' [correction]: The correction (format: s/typo/replacement).\n'\
-            '   [original]: The original message before correction.\n'\
-            '    [pattern]: The "typo" portion of the correction.\n'\
-            '[replacement]: The "replacement" portion of the correction.\n'\
-            '  [timestamp]: The timestamp of the original message.\n')
+            'print_format', 'Format string for the printed corrections.')
 
     weechat.config_set_desc_plugin(
-            'print_limit',
-            'Maximum number of lines to correct and print to the buffer.  '\
-            'If set to 0 all lines that match the pattern will be printed.')
+            'print_limit', 'Maximum number of lines to correct.')
 
     return weechat.WEECHAT_RC_OK
 
